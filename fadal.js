519b47f539119e6fb00e940658640d899e44d822
fadaldrame97-sys <fadaldrame97@gmail.com>
2025-09-23 15:08:35 +0100

    Add files via upload

---
+++ fadal.js
@@ -0,0 +1,11 @@
+
+let nom =("Drame");
+console.log(nom);
+let prenom =("fadal");
+console.log(prenom);
+let age =("26");
+console.log(age);
+let sexe =("M");
+console.log(sexe);
+let numeroPhone =("21255355367");
+console.log(numeroPhone);
